cc00a9d5d7e1857b6235d5fd4550964587bed196
master <master@DESKTOP-FD332SP>
2018-10-24 06:33:52 +0800

    Signed-off-by: master <master@DESKTOP-FD332SP>

---
+++ springbootpro/src/main/java/com/fantasy/che/springbootpro/SpringbootproApplication.java
@@ -0,0 +1,12 @@
+package com.fantasy.che.springbootpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootproApplication.class, args);
+	}
+}
